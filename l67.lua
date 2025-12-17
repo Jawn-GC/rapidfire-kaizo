b8544381afacd69531ef6d1549295642761b540f
Jonathan Rodriguez <123908160+Jawn-GC@users.noreply.github.com>
2025-12-17 01:39:50 -0800

    Add files via upload

---
+++ l67.lua
@@ -0,0 +1,91 @@
+local level_var = {
+    identifier = "l67",
+    title = "Floor 67",
+    theme = THEME.TIDE_POOL,
+    world = 1,
+	level = 67,
+	width = 2,
+    height = 3,
+    file_name = "l67.lvl",
+}
+
+local level_state = {
+    loaded = false,
+    callbacks = {},
+}
+
+level_var.load_level = function()
+    if level_state.loaded then return end
+    level_state.loaded = true
+	
+	level_state.callbacks[#level_state.callbacks+1] = set_post_entity_spawn(function(entity, spawn_flags)
+		entity:destroy()
+	end, SPAWN_TYPE.SYSTEMIC, 0, ENT_TYPE.ITEM_PICKUP_SKELETON_KEY)
+	
+	level_state.callbacks[#level_state.callbacks+1] = set_post_entity_spawn(function(entity, spawn_flags)
+		entity:destroy()
+	end, SPAWN_TYPE.ANY, 0, ENT_TYPE.MONS_SKELETON)
+
+	level_state.callbacks[#level_state.callbacks+1] = set_post_entity_spawn(function(entity, spawn_flags)
+		entity:destroy()
+	end, SPAWN_TYPE.ANY, 0, ENT_TYPE.ITEM_SKULL)
+	
+	level_state.callbacks[#level_state.callbacks+1] = set_post_entity_spawn(function(entity, spawn_flags)
+		entity:destroy()
+	end, SPAWN_TYPE.ANY, 0, ENT_TYPE.ITEM_BONES)
+
+	level_state.callbacks[#level_state.callbacks+1] = set_post_entity_spawn(function (entity)
+		entity.flags = set_flag(entity.flags, 6)
+    end, SPAWN_TYPE.ANY, 0, ENT_TYPE.FLOOR_GENERIC)
+
+	level_state.callbacks[#level_state.callbacks+1] = set_post_entity_spawn(function (entity)
+		entity.flags = set_flag(entity.flags, 6)
+    end, SPAWN_TYPE.ANY, 0, ENT_TYPE.FLOORSTYLED_STONE)
+
+	level_state.callbacks[#level_state.callbacks+1] = set_post_entity_spawn(function (entity)
+		entity.flags = set_flag(entity.flags, 6)
+    end, SPAWN_TYPE.ANY, 0, ENT_TYPE.FLOOR_THORN_VINE)
+
+	level_state.callbacks[#level_state.callbacks+1] = set_post_entity_spawn(function (entity)
+		entity.flags = set_flag(entity.flags, 6)
+    end, SPAWN_TYPE.ANY, 0, ENT_TYPE.FLOOR_CONVEYORBELT_LEFT)
+
+	level_state.callbacks[#level_state.callbacks+1] = set_post_entity_spawn(function (entity)
+		entity.flags = set_flag(entity.flags, 6)
+    end, SPAWN_TYPE.ANY, 0, ENT_TYPE.FLOOR_CONVEYORBELT_RIGHT)
+
+	level_state.callbacks[#level_state.callbacks+1] = set_post_entity_spawn(function (entity)
+        -- Remove Hermitcrabs
+        local x, y, layer = get_position(entity.uid)
+        local floor = get_entities_at(0, MASK.ANY, x, y, layer, 1)
+        if #floor > 0 then
+            entity.flags = set_flag(entity.flags, ENT_FLAG.INVISIBLE)
+            entity:destroy()
+        end
+    end, SPAWN_TYPE.ANY, 0, ENT_TYPE.MONS_HERMITCRAB)
+
+	level_state.callbacks[#level_state.callbacks+1] = set_post_entity_spawn(function(entity, spawn_flags)
+		entity:give_powerup(ENT_TYPE.ITEM_POWERUP_SPIKE_SHOES)
+	end, SPAWN_TYPE.ANY, 0, ENT_TYPE.MONS_OCTOPUS)
+
+	local frames = 0
+	level_state.callbacks[#level_state.callbacks+1] = set_callback(function ()
+		frames = frames + 1		
+    end, ON.FRAME)
+
+	toast(level_var.title)
+	
+end
+
+level_var.unload_level = function()
+    if not level_state.loaded then return end
+
+    local callbacks_to_clear = level_state.callbacks
+    level_state.loaded = false
+    level_state.callbacks = {}
+    for _, callback in pairs(callbacks_to_clear) do
+        clear_callback(callback)
+    end
+end
+
+return level_var
